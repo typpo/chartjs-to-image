887a51498e8b229cfb5a83574778301f4047fb1a
Antoine Kingue <54948363+AntoineKM@users.noreply.github.com>
2023-05-14 02:47:02 +0200

    Add types declarations (#29)
    * Add types declarations

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+declare class ChartJsImage {
+  constructor(apiKey: string, accountId: string);
+
+  setConfig(chartConfig: object): this;
+  setWidth(width: number | string): this;
+  setHeight(height: number | string): this;
+  setBackgroundColor(color: string): this;
+  setDevicePixelRatio(ratio: number | string): this;
+  setFormat(fmt: string): this;
+  setChartJsVersion(version: string): this;
+  isValid(): boolean;
+  getUrl(): string;
+  getPostData(): {
+    width: number;
+    height: number;
+    chart: string;
+    format?: string;
+    backgroundColor?: string;
+    devicePixelRatio?: number;
+    version?: string;
+  };
+  getShortUrl(): Promise<string>;
+  toBinary(): Promise<Buffer>;
+  toDataUrl(): Promise<string>;
+  toFile(pathOrDescriptor: string): Promise<void>;
+}
+
+export = ChartJsImage;
